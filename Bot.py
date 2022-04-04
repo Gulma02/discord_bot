d0d691e1f3e0b6743c512d2ae96fcd9a4372c4f8
José Gulmanelli <jgulmanelli@gmail.com>
2022-04-04 17:55:41 -0300

    FEAT: Conectarse y desconectarse de llamadas, saludar y primer version de "ruleta" y "play"

--- Bot.py
+++ Bot.py
@@ -1,4 +1,13 @@
+from discord.ext import commands
+import discord
+import nacl
+
 class Bot:
     def __init__(self):
-        self.token = 'OTYwNTYxNTAzMTI2MjkwNDYy.YksOnA._irPuVEiGk3ERGCU0NlpylSAwl0'
+        self.token = 'OTYwNTYxNTAzMTI2MjkwNDYy.YksOnA.VJwzwWkY8aNRvkRn2yKvC9JVtrY'
+        self.server_id = 960597008484286584
         self.identifier = '.'
+        # Crea el cliente de Discord para poder interactuar con el bot
+        self.client = commands.Bot(command_prefix = '.')
+        self.client_helper = discord.Client()
+        self.list_usuarios = []
--- main.py
+++ main.py
@@ -1,24 +1,58 @@
-import discord
+import discord.errors
+
 from Bot import Bot
 
 # Recupera la información privada del Bot almacenada en ese archivo
-bot_info = Bot()
-# Crea el cliente de Discord para poder interactuar con el bot
-client = discord.Client()
+bot = Bot()
 
 # @client.event es una función hecha para reaccionar a cualquier evento que reciba el bot
-@client.event
+@bot.client.event
 async def on_ready():
-    print(client)
-    print('Logged in as {0}'.format(client.user))
+    print('Logged in as {0}'.format(bot.client.user))
 
+@bot.client.event
+async def on_voice_state_update(member, before, after):
+    id = member.id
+    if id not in bot.list_usuarios:
+        bot.list_usuarios.append(member.id)
+    else:
+        bot.list_usuarios.remove(id)
+
+    print(bot.list_usuarios)
+
+## COMANDOS
 # Reacciona cuando el bot recibe un mensaje
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.client.command(name = 'saludo')
+async def saludo(message):
+    await message.channel.send('Hello ' + message.author.mention)
 
-    if message.content.startswith(bot_info.identifier):
-        await message.channel.send('Hello!')
+@bot.client.command(name = 'join')
+async def unirse(message):
+    if not message.author.voice:
+        await message.channel.send('Primero tenes que estar conectado a un canal')
+    else:
+        try:
+            canal = message.author.voice.channel
+            await canal.connect()
+        except discord.errors.ClientException:
+            await message.channel.send('Ya estoy conectado a un canal de voz :(')
 
-client.run(bot_info.token)
+@bot.client.command(name = 'leave')
+async def irse(message):
+    if message.voice_client:
+        await message.voice_client.disconnect()
+
+@bot.client.command(name = 'ruleta')
+async def ruleta(message):
+        server = bot.client_helper.get_guild(bot.server_id)
+        print(server)
+
+@bot.client.command(name = 'p')
+async def poner_musica(message, url: str):
+    canal = message.author.voice.channel
+    vc = await canal.connect()
+
+    player = await vc.create_ytdl_player(url)
+    player.start()
+
+bot.client.run(bot.token)
